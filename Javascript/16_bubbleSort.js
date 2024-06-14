f59f13c96995164f16c50fb22c5354f3591337df
Satish Hargod <satish.hargod@esparkbizmail.com>
2024-06-14 17:37:40 +0530

    add more data

--- Javascript/16_bubbleSort.js
+++ Javascript/16_bubbleSort.js
@@ -28,19 +28,19 @@ let arrn = [5, 3, 8, 4, 2];
 console.log(bubbleSort(arrn)); // Output: [2, 3, 4, 5, 8]
 
 
-
-function bubbleSort(arr){
-  for (const [i, ivalue] of arr.entries()) {
-      for (const [j, jvalue] of arr.entries()) {
-          if(arr[j] > arr[j+1]){
-              let temp = arr[j]
-              arr[j] = arr[j+1]
-              arr[j+1] = temp
+function bubbleSort(arr) {
+  for (const iterator of arr) {
+      for (const [key, value] of arr.entries()) {
+          if(arr[key] > arr[key+1]){
+              let temp = arr[key]
+              arr[key] = arr[key+1]
+              arr[key+1] = temp
           }
       }
   }
   return arr
 }
 
-let arr = [5, 3, 8, 4, 2];
+let arr = [5, 3, 8, 4, 2, 100, 86, 90];
+//  let arr = ['sas', 'as', 'newt', 'raj', 'aa'];
 console.log(bubbleSort(arr)); // Output: [2, 3, 4, 5, 8]
